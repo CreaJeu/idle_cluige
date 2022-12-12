da5d20d34a802e7a76c8c7e031a685d486b59d8c
c-illy <cillweb@gmail.com>
2022-12-12 16:53:17 +0100

    tests for deleteNode() fix

--- main_idle_cluige.c
+++ main_idle_cluige.c
@@ -14,15 +14,29 @@ int main()
 
 	//here hardcode or parse my scene from file
 	//...
-	Node n;
-	iCluige.iNode.initZero(&n);
-	iCluige.iNode.addChild(iCluige.publicRoot2D, &n);
+	Node* n = iCluige.iNode.newNode();
+	iCluige.iNode.setName(n, L"Idle Root");
+	iCluige.iNode.addChild(iCluige.publicRoot2D, n);
+
+	//don't allocate nodes on stack, see deleteNode()
+	//Node n;
+	//iCluige.iNode.initZero(&n);
+
+	//warning about node names:
+	//hardcoded strings in the form char* s = "hardcoded"
+	//are instanciated on the stack
+	//don't do n->name = s, use instead setName(n, s)
+//	wchar_t* plouf = iCluige.checkedMalloc(40 * sizeof(wchar_t));
+//	plouf[0]=L'b';plouf[1]=L'o';plouf[2]=L'b';plouf[3]=L'\0';//OK
+//	//plouf = L"bob";// <-- error on free() in deleteNode()
+//	wprintf(L"%ls\n", plouf);
+//	free(plouf);//test either plouf = "..." or plouf[0] = ...
 
 	Node* pn;
 	for(int i=0; i<3; i++)
     {
         pn = iCluige.iNode.newNode();
-        iCluige.iNode.addChild(&n, pn);
+        iCluige.iNode.addChild(n, pn);
         Node* pn01 = iCluige.iNode.newNode();
         iCluige.iNode.addChild(pn, pn01);
     }//test on a gamer pc = 2minutes for 123456 iterations
@@ -34,6 +48,10 @@ int main()
 	wprintf(L"\ntree : \n");
 	iCluige.iNode.printTreePretty(iCluige.privateRoot2D);
 
+
+	iCluige.iNode.setName(n, L"Test Rename");
+	wprintf(L"\ntree after root renamed: \n");
+	iCluige.iNode.printTreePretty(iCluige.privateRoot2D);
 
     //game loop
 	cluigeRun();
